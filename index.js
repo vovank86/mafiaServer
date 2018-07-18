c1bdd2e1bb61da3454161cde7cd4f1419e8e789e
vovank86 <vovank86@gmail.com>
2018-07-18 23:07:40 +0300

    add packages

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import * as express from 'express';
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+const router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
